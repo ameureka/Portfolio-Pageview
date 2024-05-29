ab3fa04effc4a1f89bc55adffb8c0a42817e571a
ameureka <166780113+ameureka@users.noreply.github.com>
2024-05-29 15:09:11 +0800

    001

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,8 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        {/* 这里是首页的面描述文字 */}
+        {/* <h2 className="text-sm text-zinc-500 ">
           I'm building{" "}
           <Link
             target="_blank"
@@ -43,7 +44,7 @@ export default function Home() {
           >
             unkey.dev
           </Link> to solve API authentication and authorization for developers.
-        </h2>
+        </h2> */}
       </div>
     </div>
   );
